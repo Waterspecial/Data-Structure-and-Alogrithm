9184c6a2f309a712c6b4b5bfb7fe42b4f34153a6
Waterspecial <boluwatifeomirinde080@gmail.com>
2022-09-05 02:17:40 +0100

    solved problems

--- Data Structure/LinkedList/removeLLElement.js
+++ Data Structure/LinkedList/removeLLElement.js
@@ -1,5 +1,9 @@
 /* 
-Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+Given the head of a linked list and an integer val, 
+remove all the nodes of the linked list that has Node.val == val, and return the new head.
+    1 -> 2 ->6 -> 3 -> 4 -> 5 -> 6 === 1 -> 2 -> 3 -> 4 -> 5
+    Input: head = [1,2,6,3,4,5,6], val = 6
+    Output: [1,2,3,4,5]
 */
 
 
