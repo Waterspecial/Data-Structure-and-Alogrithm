9bb6b8cf1cf2101392cb628e06004684a5b6a125
Waterspecial <boluwatifeomirinde080@gmail.com>
2022-09-22 23:46:59 +0100

    updates

---
+++ Algorithm/sliding windows/LongestRepeatingCharacterReplacement.js
@@ -0,0 +1,41 @@
+/*
+Longest Repeating Character Replacement
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+
+ */
+
+var characterReplacement = function(s, k) {
+    let windowStart = 0,
+    windowEnd = 0,
+    maxRep = 0,
+    maxLength = 0,
+    freq = {};
+
+    while(windowEnd  < s.length) {
+        let right = s[windowEnd]
+        freq[right] = freq[right] + 1 || 1;
+        maxRep =  Math.max(maxRep, freq[right]);
+
+        if((windowEnd - windowStart + 1 - maxRep) > k) {
+            let left = s[windowStart];
+            freq[left] -= 1;
+            windowStart += 1;
+        }
+        maxLength = Math.max(maxLength, windowsEnd - windowStart + 1);
+        windowEnd += 1;
+    }
+    return maxLength;
+}
