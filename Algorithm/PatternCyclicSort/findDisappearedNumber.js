08b3fcd3aba1b6d15bcad4e2b11778e2849e89d5
Waterspecial <boluwatifeomirinde080@gmail.com>
2023-01-02 20:26:40 +0100

    added

---
+++ Algorithm/PatternCyclicSort/findDisappearedNumber.js
@@ -0,0 +1,33 @@
+/**
+ * 448. Find All Numbers Disappeared in an Array
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ */
+
+const findDisappearedNumber = (nums) => {
+  let i = 0;
+  while (i < nums.length) {
+    const j = nums[i] - 1;
+    if (nums[i] !== nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    } else {
+      i += 1;
+    }
+  }
+  const res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      res.push(i + 1);
+    }
+  }
+  return res;
+};
---
+++ Algorithm/PatternCyclicSort/findTheDuplicate.js
@@ -0,0 +1,34 @@
+/**
+ 287. Find the Duplicate Number
+ Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ */
+
+var findDuplicate = (nums) => {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] !== i + 1) {
+      const j = nums[i] - 1;
+      if (nums[i] !== nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      } else {
+        return nums[i];
+      }
+    } else {
+      i += 1;
+    }
+  }
+  return -1
+};
