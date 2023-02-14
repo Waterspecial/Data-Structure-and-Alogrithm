c99e023576fb42a841ffca3ef617540edc67ae1c
Boluwatife Omirinde <44232396+Waterspecial@users.noreply.github.com>
2023-02-15 00:22:33 +0100

    Create DiagonalTraverse.js
    Updates

---
+++ Data Structure/ArraysStrings/DiagonalTraverse.js
@@ -0,0 +1,15 @@
+  let rows = mat.length;
+    let cols = mat[0].length;
+    const result = new Array(rows + cols -1).fill(null).map(() => []);
+    
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if((row + col) % 2 === 0) {
+                result[row + col].unshift(mat[row][col]);
+            }else{
+                result[row + col].push(mat[row][col]);
+            }
+        }
+    }
+    
+    return result.flat()
